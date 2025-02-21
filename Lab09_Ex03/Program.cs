cf85a1b195e90f543a3b515062ed379fe8134b07
Panyayakob <Turkeeyakob@gmail.com>
2025-02-21 15:40:22 +0700

    ส่งงาน

---
+++ Lab09_Ex03/Program.cs
@@ -0,0 +1,19 @@
+﻿﻿Person.name = "Panya"; 
+System.Console.WriteLine(Person.GetName());  
+var student = new Student();
+Student.name = "66030109";
+Console.WriteLine(Student.GetName());
+
+class Person
+{
+    public static string? name;  
+    public static string GetName()
+    {
+        return $"Hello from {name}";
+    }
+}
+
+class Student : Person
+{
+    
+}
